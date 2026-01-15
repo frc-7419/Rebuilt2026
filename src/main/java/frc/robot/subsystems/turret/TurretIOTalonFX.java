0dbd5b7502947883be452ffe0ec90320ca11bf50
FalconWings <azni.raza@gmail.com>
2026-01-15 12:27:30 -0800

    Updated Turret
    Updated Turret to include two serializer motors

--- src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
@@ -1,77 +1,91 @@
-// Copyright (c) 2021-2026 Littleton Robotics
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by a BSD
-// license that can be found in the LICENSE file
-// at the root directory of this project.
+private final TalonFX motor;
+private final TalonFX launcher;
+private final TalonFX shooter;
 
-package frc.robot.subsystems.turret;
+private final StatusSignal<Voltage> motorAppliedVolts, launcherAppliedVolts, shooterAppliedVolts;
+private final StatusSignal<Current> motorCurrent, launcherCurrent, shooterCurrent;
+private final StatusSignal<AngularVelocity> motorVelocity, launcherVelocity, shooterVelocity;
+private final StatusSignal<edu.wpi.first.units.measure.Angle> motorPosition, launcherPosition, shooterPosition;
 
-import static frc.robot.util.PhoenixUtil.tryUntilOk;
+private final VoltageOut voltageRequest = new VoltageOut(0);
+private final PositionVoltage positionVoltageRequest = new PositionVoltage(0.0);
 
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusCode;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Current;
-import edu.wpi.first.units.measure.Voltage;
-import frc.robot.generated.TunerConstants;
-
-/** TalonFX implementation of TurretIO. */
-public class TurretIOTalonFX implements TurretIO {
-  private final TalonFX motor;
-
-  private final StatusSignal<Voltage> motorAppliedVolts;
-  private final StatusSignal<Current> motorCurrent;
-  private final StatusSignal<AngularVelocity> motorVelocity;
-  private final StatusSignal<edu.wpi.first.units.measure.Angle> motorPosition;
-
-  // Control requests
-  private final VoltageOut voltageRequest = new VoltageOut(0);
-  private final PositionVoltage positionVoltageRequest = new PositionVoltage(0.0);
-
-  public TurretIOTalonFX(int canId) {
-    motor = new TalonFX(canId, TunerConstants.kCANBus);
+public TurretIOTalonFX(int motorCanId, int launcherCanId, int shooterCanId) {
+    motor = new TalonFX(motorCanId, TunerConstants.kCANBus);
+    launcher = new TalonFX(launcherCanId, TunerConstants.kCANBus);
+    shooter = new TalonFX(shooterCanId, TunerConstants.kCANBus);
 
     var config = new TalonFXConfiguration();
     config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
     tryUntilOk(5, () -> motor.getConfigurator().apply(config, 0.25));
+    tryUntilOk(5, () -> launcher.getConfigurator().apply(config, 0.25));
+    tryUntilOk(5, () -> shooter.getConfigurator().apply(config, 0.25));
 
     motorAppliedVolts = motor.getMotorVoltage();
     motorCurrent = motor.getStatorCurrent();
     motorVelocity = motor.getVelocity();
     motorPosition = motor.getPosition();
 
-    BaseStatusSignal.setUpdateFrequencyForAll(
-        50.0, motorAppliedVolts, motorCurrent, motorVelocity, motorPosition);
-  }
+    launcherAppliedVolts = launcher.getMotorVoltage();
+    launcherCurrent = launcher.getStatorCurrent();
+    launcherVelocity = launcher.getVelocity();
+    launcherPosition = launcher.getPosition();
 
-  @Override
-  public void updateInputs(TurretIOInputs inputs) {
-    var status =
-        BaseStatusSignal.refreshAll(motorAppliedVolts, motorCurrent, motorVelocity, motorPosition);
+    shooterAppliedVolts = shooter.getMotorVoltage();
+    shooterCurrent = shooter.getStatorCurrent();
+    shooterVelocity = shooter.getVelocity();
+    shooterPosition = shooter.getPosition();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50.0,
+        motorAppliedVolts, motorCurrent, motorVelocity, motorPosition,
+        launcherAppliedVolts, launcherCurrent, launcherVelocity, launcherPosition,
+        shooterAppliedVolts, shooterCurrent, shooterVelocity, shooterPosition);
+}
+
+@Override
+public void updateInputs(TurretIOInputs inputs) {
+    var status = BaseStatusSignal.refreshAll(
+        motorAppliedVolts, motorCurrent, motorVelocity, motorPosition,
+        launcherAppliedVolts, launcherCurrent, launcherVelocity, launcherPosition,
+        shooterAppliedVolts, shooterCurrent, shooterVelocity, shooterPosition);
+
     inputs.connected = status.equals(StatusCode.OK);
+
     inputs.appliedVolts = motorAppliedVolts.getValueAsDouble();
     inputs.currentAmps = motorCurrent.getValueAsDouble();
     inputs.velocityRadPerSec = Units.rotationsToRadians(motorVelocity.getValueAsDouble());
     inputs.position = Rotation2d.fromRotations(motorPosition.getValueAsDouble());
-    inputs.absolutePosition = inputs.position; // no separate absolute sensor
-  }
+    inputs.absolutePosition = inputs.position;
 
-  @Override
-  public void setOpenLoop(double volts) {
+    inputs.launcherVolts = launcherAppliedVolts.getValueAsDouble();
+    inputs.launcherCurrent = launcherCurrent.getValueAsDouble();
+    inputs.launcherVelocityRadPerSec = Units.rotationsToRadians(launcherVelocity.getValueAsDouble());
+    inputs.launcherPosition = Rotation2d.fromRotations(launcherPosition.getValueAsDouble());
+
+    inputs.shooterVolts = shooterAppliedVolts.getValueAsDouble();
+    inputs.shooterCurrent = shooterCurrent.getValueAsDouble();
+    inputs.shooterVelocityRadPerSec = Units.rotationsToRadians(shooterVelocity.getValueAsDouble());
+    inputs.shooterPosition = Rotation2d.fromRotations(shooterPosition.getValueAsDouble());
+}
+
+@Override
+public void setOpenLoop(double volts) {
     motor.setControl(voltageRequest.withOutput(volts));
-  }
+}
 
-  @Override
-  public void setPosition(Rotation2d position) {
+@Override
+public void setPosition(Rotation2d position) {
     motor.setControl(positionVoltageRequest.withPosition(position.getRotations()));
-  }
+}
+
+@Override
+public void setLauncherOpenLoop(double volts) {
+    launcher.setControl(voltageRequest.withOutput(volts));
+}
+
+@Override
+public void setShooterOpenLoop(double volts) {
+    shooter.setControl(voltageRequest.withOutput(volts));
 }
